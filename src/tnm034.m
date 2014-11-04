98c91a22499d791d1663a0816069268fb7466371
Pär Eriksson <parer800@student.liu.se>
2014-11-04 09:07:55 +0100

    added initial sceleton

---
+++ src/tnm034.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%% 
+function strout = tnm034(im) 
+% 
+% Im: Input image of captured sheet music. Im should be in
+% double format, normalized to the interval [0,1] 
+% strout: The resulting character string of the coded 
+% The string must have exactly the number of characters that 
+% corresponds to the information in the code. 
+%
+% Your program code.
+%
+strout=char(string);
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
