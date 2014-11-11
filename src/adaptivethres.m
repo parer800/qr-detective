5b7314ca04b497dd8ebf750b0a566322f28befa2
Pär Eriksson <parer800@student.liu.se>
2014-11-11 09:35:57 +0100

    Added adaptive thresholding, tried something with hough transform but it shouldn't be used

---
+++ src/adaptivethres.m
@@ -0,0 +1,53 @@
+%Extension of the Weller's method [Wellner 1993]
+
+function bw = adaptivethres(in)
+    w = size(in,1)
+    h = size(in,2)
+    %calculate integral image
+    intImg = integralImage(in);
+    s=w/8;
+    t = 15;
+    
+    for i=1:w
+        for j=1:h
+            sHalf = floor(s/2);
+            x1 = i-(sHalf);
+            x2 = i+(sHalf);
+            y1 = j-sHalf;
+            y2 = j+sHalf;
+            
+            if x1 < 2
+                x1 = i+1;
+            end
+            if y1 < 2
+                y1 = j+1;
+            end
+            if i==1
+                x1=2;
+            end
+            if j==1
+                y1=2;
+            end
+            if x2 > w
+                x2 = w;
+            end
+            if y2 > h
+                y2 = h;
+            end
+            count = (x2-x1)*(y2-y1);
+            sum = intImg(x2,y2) - intImg(x2,y1-1) - intImg(x1-1,y2) + intImg(x1-1,y1-1);
+            if in(i,j)*count <= (sum*(100-t)/100)
+                bw(i,j) = 0;
+            else
+                bw(i,j) = 255;
+            end
+        end
+    end
+    imshow(bw);
+    figure;
+    imshow(in);
+    
+    %imshow(bw);
+    
+    
+
--- src/tnm034.m
+++ src/tnm034.m
@@ -9,6 +9,100 @@ function strout = tnm034(im)
 %
 % Your program code.
 %
-strout=char(string);
+
+I = imread(im);
+
+%Get dimensions, to check if 
+[x,y,z] = size(I);
+if z == 3
+    z
+    BW = rgb2gray(I);
+    G = fspecial('disk',15);
+    %Ig = imfilter(BW,G,'same');
+    
+    %BW = edge(BW, 'canny');
+else
+    z
+    BW = I;
+    %BW = edge(BW, 'canny');
+end
+%adaptive threshold image
+atImage = adaptivethres(double(BW));
+
+
+[H, T, R] = hough(atImage);
+
+
+% Display the original image.
+subplot(2,1,1);
+figure;
+
+title(im);
+
+%LOOP THROUGH AND SAVE on every color switch, position (x,y), counter, pixelcolor. 
+width = size(atImage,2);
+heigth = size(atImage,1);
+
+segment = [0,0,0,0];
+color_switch = atImage(1,1);
+sequence_counter = 0;
+for y=1:height
+    current_v = atImage(y,x);
+    
+    for x=1:width
+        %IF color has switched, store previous sequence in segment
+        current_h = atImage(y,x);
+        
+        if current_h ~= color_switch
+            sequence_counter = sequence_counter + 1;
+            
+        end
+        
+    end
+end
+
+
+
+
+
+% Display & calculate the Hough matrix.
+% 
+% P  = houghpeaks(H,5);
+% 
+% subplot(2,1,2);
+% imshow(imadjust(mat2gray(H)),'XData',T,'YData',R,...
+%       'InitialMagnification','fit');
+% title('Hough Transform of Image');
+% xlabel('\theta'), ylabel('\rho');
+% axis on, axis normal, hold on;
+% plot(T(P(:,2)),R(P(:,1)),'s','color','red');
+% %colormap(hot);
+% lines = houghlines(BW,T,R,P,'FillGap',5,'MinLength',7);
+% figure, imshow(atImage), hold on
+% max_len = 0;
+% for k = 1:length(lines)
+%    xy = [lines(k).point1; lines(k).point2];
+%    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+% 
+%    % Plot beginnings and ends of lines
+%    plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+%    plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+% 
+%    % Determine the endpoints of the longest line segment
+%    len = norm(lines(k).point1 - lines(k).point2);
+%    if ( len > max_len)
+%       max_len = len;
+%       xy_long = xy;
+%    end
+% end
+% 
+% % highlight the longest line segment
+% plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','blue');
+
+
+
+
+
+strout=char(im);
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
