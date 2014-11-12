78acc5e767cd843a8c38c4d98e03cd5f981c0414
Pär Eriksson <parer800@student.liu.se>
2014-11-12 11:23:09 +0100

    storing position, color, and nrOf previous color, for every color switch. These are now stored in a 1D array but will be refactored to a 4xN matrix

--- src/tnm034.m
+++ src/tnm034.m
@@ -41,25 +41,74 @@ title(im);
 
 %LOOP THROUGH AND SAVE on every color switch, position (x,y), counter, pixelcolor. 
 width = size(atImage,2);
-heigth = size(atImage,1);
+height = size(atImage,1);
 
-segment = [0,0,0,0];
+%Store in block of 4: [x,y,colorSeq, counterSeq]
+segment = -ones(1,height*width);
 color_switch = atImage(1,1);
 sequence_counter = 0;
+ticker = 1;
 for y=1:height
-    current_v = atImage(y,x);
-    
+    %current_v = atImage(y,x);
+    sequence_counter = 0;
     for x=1:width
         %IF color has switched, store previous sequence in segment
         current_h = atImage(y,x);
-        
         if current_h ~= color_switch
+            %Color has switched
+            component = [x,y,color_switch, sequence_counter]
+            segment(1,ticker:ticker+3) = component;
+            ticker = ticker+4;
+            sequence_counter = 0;
+            color_switch = current_h;
+        end
             sequence_counter = sequence_counter + 1;
             
-        end
         
     end
 end
+x = (0);
+qr_locations = double.empty();
+for i=1:1:width*height
+    if segment(1,i) == -1 && segment(1,i+4) == -1
+        break;
+    end
+    sum = 0;
+    first_color = segment(i+2);
+    position = 0;
+    for j=0:4        
+        sum = sum + segment(i+3+j*4); % Get every counter value, add that to sum. check if modulus(sum, 7) is equal to 0 
+    end
+    
+    position = [segment(i+8); segment(i+9)];
+    if(mod(sum,7) == 0)
+        center_weight = segment(i+11);
+        if(center_weight/(sum-center_weight) ~= (0.75))
+            continue;
+        end
+        %Hmm interesting.. found correct counter sequence but mabye not the
+        % corerct color sequence. Since the colors are always switched we
+        % only need to look at the first color value which should be black
+        % Since we have a binary image we look for c ==0 || c==0.0
+       % if first_color == 0 || first_color == 0.0
+            %Located QR code!!
+            qr_locations = [qr_locations, position];
+        %end
+    end
+    
+    
+end
+figure;
+imshow(atImage);
+hold on;
+size(qr_locations,2)
+for i=1:size(qr_locations,2)
+    plot(qr_locations(1,i),qr_locations(2,i), '+');
+end
+
+%imshow(segment(:,ticker+4));
+
+
 
 
 
